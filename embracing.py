f6886aee96d1a694392db493325d9cf0f4cebc2a
tsumarios <mraciti96@gmail.com>
2023-04-21 10:40:07 +0200

    Update step4, add cardinality 3 scores

--- embracing.py
+++ embracing.py
@@ -9,6 +9,7 @@ import spacy
 import warnings
 from nltk.corpus import wordnet as wn
 from sentence_transformers import SentenceTransformer, util
+from statistics import mean
 
 
 warnings.filterwarnings('ignore')
@@ -198,11 +199,15 @@ def step4(preliminary_path: str, scores_path: str, k=3) -> tuple:
     '''
     Gathers sentences per groups of k elements, then retrieve the previously computed similarity scores.
     '''
+    if k < 3:
+        print('Please specify a cardinality greater or equal to 3.')
+        return
+
     ss_df = pandas.read_csv(scores_path)
     sentence_list= pandas.read_csv(preliminary_path, index_col='P')['LB'].values.tolist()
     ordered_sentences = list(itertools.combinations(sentence_list, k))   # binomial coefficient yields the total number of groups, where n = len(sentence_list)
     
-    scores_dict = {'scores': []}
+    scores_dict = {'max': [], 'mean': [], 'min': [], 'scores': []}
     for i in range(1, k+1):
         scores_dict[f'sentence{i}'] = []
     
@@ -210,11 +215,14 @@ def step4(preliminary_path: str, scores_path: str, k=3) -> tuple:
         pairs = list(itertools.combinations(tupl,2))
         scores = []
         for p in pairs:
-            score = ss_df.iloc[((ss_df['sentence1'].values==p[0]) & (ss_df['sentence2'].values==p[1])) | ((ss_df['sentence1'].values==p[1]) & (ss_df['sentence2'].values==p[0]))]['score'].values[0]
-            scores.append(score)
+            scores.append(ss_df.iloc[((ss_df['sentence1'].values==p[0]) & (ss_df['sentence2'].values==p[1])) |
+                                     ((ss_df['sentence1'].values==p[1]) & (ss_df['sentence2'].values==p[0]))]['score'].values[0])
         for i in range(1, k+1):
             scores_dict[f'sentence{i}'].append(tupl[i-1])
         scores_dict['scores'].append(scores)
+        scores_dict['max'].append(max(scores))
+        scores_dict['mean'].append(mean(scores))
+        scores_dict['min'].append(min(scores))
 
     semantic_similarity_scores = pandas.DataFrame(scores_dict)
-    semantic_similarity_scores.to_csv(f'{preliminary_path.split(".csv")[0]}_ss_scores_by_{k}.csv', index=False, encoding='utf-8')
+    semantic_similarity_scores.to_csv(f'{preliminary_path.split(".csv")[0]}_ss_scores_with_cardinality_{k}.csv', index=False, encoding='utf-8')
